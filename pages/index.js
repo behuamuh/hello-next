f48d7c2d5401c6e941114fa11a93efadef9e70a6
Behuamuh <sven1980@list.ru>
2019-01-19 13:50:46 +0300

    learn link property -as

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,21 @@
-import Layout from "../components/MyLayout";
+import Layout from "../components/MyLayout.js";
 import Link from "next/link";
 
-const PostLink = ({ title }) => (
+const PostLink = props => (
   <li>
-    <Link href={`/post?title=${title}`}>
-      <a>{title}</a>
+    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
     </Link>
   </li>
 );
 
-const Index = () => (
+export default () => (
   <Layout>
     <h1>My Blog</h1>
     <ul>
-      <PostLink title="Hello Next.js" />
-      <PostLink title="Learn Next.js is awesome" />
-      <PostLink title="Deploy apps with Zeit" />
+      <PostLink id="hello-nextjs" title="Hello Next.js" />
+      <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
+      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
     </ul>
   </Layout>
 );
-
-export default Index;
